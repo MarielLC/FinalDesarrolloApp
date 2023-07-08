f598cd3222588fe4b3becdef332a093e2e142940
DavidDG21 <los.pc.13@gmail.com>
2023-07-07 19:31:39 -0500

    dar funcionalidad al login registrer

---
+++ app/src/main/java/xyz/android/appdesarrollo/operaciones_matrices.java
@@ -0,0 +1,14 @@
+package xyz.android.appdesarrollo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class operaciones_matrices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operaciones_matrices);
+    }
+}
